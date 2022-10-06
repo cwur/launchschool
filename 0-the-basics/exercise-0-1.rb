930048f76c8aa88a0e6307f64d63981222c995e1
cwur <cwur@mailbox.org>
2022-10-06 20:36:34 +0200

    Fix exercise 0-1

--- 0-the-basics/exercise-0-1.rb
+++ 0-the-basics/exercise-0-1.rb
@@ -1,2 +1,7 @@
-puts "Brad" + "Pit"
-puts "Brad" + 'Pit'
+puts "Brad" + " " + "Pitt"
+
+first_name = "Brad"
+last_name = "Pitt"
+
+puts first_name + " " + last_name
+puts "#{first_name} #{last_name}"
