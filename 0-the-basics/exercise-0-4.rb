a1fc629c92d7d4f0ef7092b81a95b1f845aa0339
cwur <cwur@mailbox.org>
2022-10-06 20:52:11 +0200

    Add exercise-0-4 (array)

---
+++ 0-the-basics/exercise-0-4.rb
@@ -0,0 +1,11 @@
+movie_dates = [1999, 2007, 1996]
+
+puts movie_dates[0]
+puts movie_dates[1]
+puts movie_dates[2]
+
+puts "#{movie_dates[0]} #{movie_dates[2]} #{movie_dates[1]}"
+
+print movie_dates[2].to_s + " "
+print movie_dates[0].to_s + " "
+print movie_dates[1].to_s + "\n"
