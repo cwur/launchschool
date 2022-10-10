7833d8bf58b20f560552d76e49e4b524d0f70c85
cwur <cwur@mailbox.org>
2022-10-10 20:37:56 +0200

    Add exercise 0-6 (squares)

---
+++ 0-the-basics/exercise-0-6.rb
@@ -0,0 +1,14 @@
+puts("Calculate the square.")
+
+print("Enter your first number: ")
+num1 = gets.chomp
+
+print("Enter your second number: ")
+num2 = gets.chomp
+
+print("Enter your third number: ")
+num3 = gets.chomp
+
+puts("square1: #{num1.to_i**2}")
+puts("square2: #{num2.to_i**2}")
+puts("square3: #{num3.to_i**2}")
