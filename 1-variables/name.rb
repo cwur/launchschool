81bee756323af81d543b5352085997e8c773bc49
cwur <cwur@mailbox.org>
2022-10-10 21:36:37 +0200

    Update exercise-1-1 (first and last name)

--- 1-variables/name.rb
+++ 1-variables/name.rb
@@ -1,6 +1,9 @@
-print("Whats your name? ")
-name = gets.chomp
+print("Whats your first name? ")
+first_name = gets.chomp
+
+print("Whats your last name? ")
+last_name = gets.chomp
 
 10.times {
-  puts("Hello #{name}")
+  puts("Hello #{first_name} #{last_name}")
 }
